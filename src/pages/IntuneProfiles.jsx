c993a978ccd2ad64e9e31951a45fdc20ba628368
lexisissy <lexisissy659@gmail.com>
2026-03-11 16:09:34 +0000

    File changes

--- src/pages/IntuneProfiles.jsx
+++ src/pages/IntuneProfiles.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { FolderCog, Pencil } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
